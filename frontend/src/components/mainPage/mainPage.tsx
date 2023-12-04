2a1d505d07f254b4c6b44916245f2950d3e9233d
LuisFraga <luiseduardofragawermann@gmail.com>
2023-12-04 07:23:11 -0300

    pratos em cima

--- frontend/src/components/mainPage/mainPage.tsx
+++ frontend/src/components/mainPage/mainPage.tsx
@@ -53,29 +53,6 @@ function mainPage() {
 			<Navbar />
 			<br />
 			<br />
-			<br />
-			<div className="container my-5 " id="restaurantes">
-				<h1 className="p-2 m-2">restaurantes</h1>
-				<div className="row my-auto">
-					{restaurantList.map((item, index) => (
-						<div key={index} className="col-2">
-							<div className="card cardHeight">
-								<img src={item.restaurantImage} alt="restaurant" className="card-img-top border p-2 restImage " />
-								<div className="card-body text-center">
-									<div className="card-subtitle border rounded">id: {item.restaurantId}</div>
-									<div className="card-title border rounded">{item.restaurantName}</div>
-									<div className="card-text border rounded">{item.address}</div>
-									<div className="card-text border rounded">{item.workTime}</div>
-									<div className="card-text border rounded">
-										<b>{item.itemList.length}</b> itens no cardapio
-									</div>
-								</div>
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-			<hr />
 			<div className="container " id="items">
 				<h1 className="p-2 m-2">pratos</h1>
 				<div className="row">
@@ -93,6 +70,28 @@ function mainPage() {
 									</div>
 									<div className="text-end col">
 										<MdOutlineAddShoppingCart size={30} className="bg-success my-2 p-1 rounded" />
+									</div>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+			<hr />
+			<div className="container my-5 " id="restaurantes">
+				<h1 className="p-2 m-2">restaurantes</h1>
+				<div className="row my-auto">
+					{restaurantList.map((item, index) => (
+						<div key={index} className="col-2">
+							<div className="card cardHeight">
+								<img src={item.restaurantImage} alt="restaurant" className="card-img-top border p-2 restImage " />
+								<div className="card-body text-center">
+									<div className="card-subtitle border rounded">id: {item.restaurantId}</div>
+									<div className="card-title border rounded">{item.restaurantName}</div>
+									<div className="card-text border rounded">{item.address}</div>
+									<div className="card-text border rounded">{item.workTime}</div>
+									<div className="card-text border rounded">
+										<b>{item.itemList.length}</b> itens no cardapio
 									</div>
 								</div>
 							</div>
